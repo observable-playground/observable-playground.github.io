d3f6ca12392075de308f2c401f9e93b915902146
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-02-13 03:41:21 +0200

    Updates e948a54 - fixing more issues with universal

---
+++ react-static-browser-plugins.js
@@ -0,0 +1,12 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/kos/playground/react-static-test",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
---
+++ react-static-templates.js
@@ -0,0 +1,27 @@
+
+import universal, { setHasBabelPlugin } from '/Users/kos/playground/react-static-test/node_modules/react-universal-component/dist/index.js'
+
+
+setHasBabelPlugin()
+
+const universalOptions = {
+  loading: () => null,
+  error: props => {
+    console.error(props.error);
+    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
+  },
+}
+
+const t_0 = universal(import('../src/pages/404.js'), universalOptions)
+const t_1 = universal(import('../src/pages/index.js'), universalOptions)
+const t_2 = universal(import('../src/containers/Library'), universalOptions)
+const t_3 = universal(import('../src/Playground/PlaygroundContainer'), universalOptions)
+
+
+// Template Map
+export default [
+  t_0,
+t_1,
+t_2,
+t_3
+]
