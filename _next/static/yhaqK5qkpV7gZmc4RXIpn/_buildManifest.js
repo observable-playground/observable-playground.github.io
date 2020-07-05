cd28f9b076886bd0c3b59e6a27f56e92ac0c02b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-05 18:00:19 +0000

    Deploy observable-playground/observable-playground.github.io to github.com/observable-playground/observable-playground.github.io.git:master

---
+++ _next/static/yhaqK5qkpV7gZmc4RXIpn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/404":["static\u002Fcss\u002F3c18aae37d21524f1074.css"],"/[libId]":[a],"/[libId]/[fileId]":["static\u002Fcss\u002Fc76a3f1c51140499d7bb.css"],"/gist/[id]":["static\u002Fcss\u002F1fbbe35965d18f22f9e1.css"],"/index":[a]}}("static\u002Fcss\u002Ff999297fc827207a9228.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
